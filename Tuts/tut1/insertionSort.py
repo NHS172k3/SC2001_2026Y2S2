e4d93338e62389ad469d348f213073318212e981
NHS172k3 <harinachiappansubramanian@gmail.com>
2026-01-25 13:48:03 +0800

    Tut1

---
+++ Tuts/tut1/insertionSort.py
@@ -0,0 +1,32 @@
+def insertionSort(A: list) -> list:
+    
+    comparisons = 0
+    for index in range(1, len(A)):
+
+        # Below swaps the element one by one --> Inefficient
+        # while index > 0 and A[index] <= A[index-1]:
+        #     A[index], A[index-1] = A[index-1] , A[index]
+        #     index -= 1
+
+        # Here we store the to-be-replaced value in a key and shift the elements until our intended element is reached
+        j, key = index, A[index]
+
+        while j > 0 and key <= A[j-1]:
+            comparisons += 1
+            A[j] = A[j-1]
+            j -= 1
+        
+        A[j] = key
+
+    
+    return A, comparisons
+
+
+
+
+if __name__ == "__main__":
+    listTest = []
+    for i in range(0, 100000):
+        listTest.append(i)
+    listTest.extend([0, -2, -4, -6, -8])
+    print(insertionSort(listTest))
---
+++ Tuts/tut1/mergeSort.py
@@ -0,0 +1,36 @@
+def merge(A: list, B: list) -> tuple:  # Return merged list and comparison count
+    res = []
+    Ap, Bp = 0, 0
+    comparisons = 0
+    while Ap < len(A) or Bp < len(B):
+        if Ap >= len(A):
+            res.append(B[Bp])
+            Bp += 1
+        elif Bp >= len(B):
+            res.append(A[Ap])
+            Ap += 1
+        elif A[Ap] >= B[Bp]:
+            comparisons += 1
+            res.append(B[Bp])
+            Bp += 1
+        else:
+            comparisons += 1
+            res.append(A[Ap])
+            Ap += 1
+    return res, comparisons
+
+
+def mergeSort(A: list) -> tuple: 
+    if len(A) <= 1:
+        return A, 0
+    mid = len(A) // 2
+    leftArr, left_comps = mergeSort(A[:mid])
+    rightArr, right_comps = mergeSort(A[mid:])
+    merged, merge_comps = merge(leftArr, rightArr)
+    total_comps = left_comps + right_comps + merge_comps
+    return merged, total_comps
+
+
+if __name__ == "__main__":
+    sorted_list, comps = mergeSort([23]*8)
+    print(sorted_list, comps)
